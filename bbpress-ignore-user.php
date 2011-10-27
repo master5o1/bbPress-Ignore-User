44bee41b7a643f2931d6327ed63394ea43a449a6
Jason <jason@master5o1.com>
2011-10-27 22:27:40 +1300

    Adding version 0.1 that was in trunk on WordPress Extend.

---
+++ bbpress-ignore-user.php
@@ -0,0 +1,277 @@
+<?php
+/**
+ * Plugin Name: bbPress Ignore User
+ * Plugin URI: http://wordpress.org/extend/plugins/bbpress-ignore-user/
+ * Description: Allow members of the forum to selectively have a user's posts and topics hidden from view.
+ * Dependencies: bbpress/bbpress.php
+ * Version: 0.1
+ * Author: Jason Schwarzenberger
+ * Author URI: http://master5o1.com/
+ */
+/*  Copyright 2011  Jason Schwarzenberger  (email : jason@master5o1.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if ( !isset($_GET['show_ignored_users']) ) {
+	add_filter( 'bbp_has_topics', array('bbp_5o1_ignore_user', 'hide_ignored_topics'), 10, 2 );
+	add_filter( 'bbp_has_replies', array('bbp_5o1_ignore_user', 'hide_ignored_replies'), 10, 2 );
+}
+
+add_action( 'wp_head', array('bbp_5o1_ignore_user', 'jquery_script') );
+
+add_action( 'wp_ajax_bbp_5o1_unignore_user', array('bbp_5o1_ignore_user', 'ajax_unignore_user') );
+add_action( 'wp_ajax_bbp_5o1_ignore_user', array('bbp_5o1_ignore_user', 'ajax_ignore_user') );
+add_filter( 'bbp_get_reply_author_link', array('bbp_5o1_ignore_user', 'show_ignore_user_button'), 10, 2 );
+add_action( 'bbp_template_notices', array('bbp_5o1_ignore_user', 'show_users_ignore_list') );
+
+// Plugin class:
+class bbp_5o1_ignore_user {
+
+	// import jQuery and set up ajaxurl so that we can use this script.
+	// Is there a better, more API-based way?  Example:
+	function jquery_script() {
+		echo '<script type="text/javascript" src="'.site_url().'/wp-includes/js/jquery/jquery.js?ver=1.4.4"></script>';
+		echo '<script type="text/javascript">var IgnoreUser = { ajaxurl: "'.site_url().'/wp-admin/admin-ajax.php" };</script>';
+	}
+	
+	function get_ignored_users($user_id = 0) {
+		global $bbp;
+		if ( empty($user_id) )
+			$user_id = $bbp->current_user->ID;
+		$raw = get_user_meta($user_id, 'bbp_5o1_ignored_users', true);
+		if ( empty($raw) )
+			return false;
+		$raw = explode(',',$raw);
+		$ignored_users = array();
+		foreach ($raw as $id) {
+			$user = get_userdata( $id );
+			if ( !user_can($user->ID, 'moderate') ) // prevent moderators and administrators from being ignored.
+				$ignored_users[$user->ID] = $user->user_login;
+		}
+		return $ignored_users;
+	}
+	
+	function set_ignored_users($user_id, $ignored_users) {
+		$before = bbp_5o1_ignore_user::get_ignored_users($user_id);
+		if ( !empty($before) )
+			delete_user_meta( $user_id, 'bbp_5o1_ignored_users' );
+		if ( empty($ignored_users) )
+			return;
+		$ignored_ids = "";
+		foreach ($ignored_users as $id => $user_login) {
+			if ( !user_can($id, 'moderate') )
+				$ignored_ids .= "," . $id;
+		}
+		$ignored_ids = substr($ignored_ids, 1);
+		if ( !empty($ignored_ids) )
+			update_user_meta( $user_id, 'bbp_5o1_ignored_users', $ignored_ids );
+	}
+
+	function hide_ignored_topics($have_posts, $topic_query) {
+		global $bbp;
+		$ignored_users = bbp_5o1_ignore_user::get_ignored_users();
+		if ( empty($ignored_users) ) // Nothing to do.
+			return $have_posts;
+		if ( in_array( $bbp->displayed_user->ID, array_keys($ignored_users) ) ) // Don't hide a user's topics if they're on their profile archive.
+			return $have_posts;
+		
+		$posts = $bbp->topic_query->posts;
+		$saved_posts = null;
+		$i = 0;
+		foreach ( $posts as $post ) {
+			$user = get_userdata($post->author_id);
+			if ( in_array($post->post_author, array_keys($ignored_users) ) ) {
+				$i++;
+				if ( user_can($post->post_author, 'moderate') ) {
+					$i--;
+					$saved_posts[] = $post;
+				}
+				continue;
+			}
+			$saved_posts[] = $post;
+		}
+		
+		$bbp->topic_query->post_count -= $i;
+		$bbp->topic_query->posts = $saved_posts;
+		
+		if ($i != 0) {
+			$code = "print '<div class=\"bbp-template-notice\">Hiding ".$i." topic".(($i!=1)?"s":"")." from users that you have chosen to ignore. <a href=\"?show_ignored_users\">Show all topics</a>.</div>';";
+			add_action( 'bbp_template_before_topics_loop' , create_function('', $code) );
+		}
+		
+		return $have_posts;
+	}
+
+	function hide_ignored_replies($have_posts, $reply_query) {
+		global $bbp;
+		$ignored_users = bbp_5o1_ignore_user::get_ignored_users();
+		if ( empty($ignored_users) ) // Nothing to do.
+			return $have_posts;
+		if ( in_array( $bbp->displayed_user->ID, array_keys($ignored_users) ) ) // Don't hide a user's replies if they're on their profile archive.
+			return $have_posts;
+		
+		$posts = $bbp->reply_query->posts;
+		$saved_posts = null;
+		$i = 0;
+		foreach ( $posts as $post ) {
+			if ( in_array($post->post_author, array_keys($ignored_users) ) ) {
+				$i++;
+				if ( user_can($post->post_author, 'moderate') ) {
+					$i--;
+					$saved_posts[] = $post;
+				}
+				continue;
+			}
+			$saved_posts[] = $post;
+		}
+		
+		$bbp->reply_query->post_count -= $i;
+		$bbp->reply_query->posts = $saved_posts;
+		
+		if ($i != 0) {
+			$code = "print '<div class=\"bbp-template-notice\">Hiding ".$i." repl".(($i!=1)?"ies":"y")." from users that you have chosen to ignore. <a href=\"?show_ignored_users\">Show all posts</a>.</div>';";
+			add_action( 'bbp_template_before_replies_loop' , create_function('', $code) );
+		}
+		
+		return $have_posts;
+	}
+	
+	function show_users_ignore_list() {
+		global $bbp;
+		// User isn't supposed to see this list here, so don't try showing it.
+		if ( !( bbp_is_user_home() || current_user_can( 'edit_users' ) ) )
+			return;
+		// We only want to show this on either the current_user's		or displayed_user's profiles.
+		if ( !in_array( substr(site_url(),0,strlen(site_url())-strlen(parse_url(site_url(),PHP_URL_PATH))) . $_SERVER['REQUEST_URI'] ,array ( bbp_get_user_profile_url($bbp->displayed_user->ID), bbp_get_user_profile_url($bbp->current_user->ID)) ) )
+			return;
+		$ignored_users = bbp_5o1_ignore_user::get_ignored_users($bbp->displayed_user->ID);
+		// User doesn't have any usernames in the list, so don't show it.
+		if ( empty($ignored_users) )
+			return;
+		$displayed_user_id = $bbp->displayed_user->ID;
+		?><table class="profile-fields zebra"> 
+			<tr> 
+				<td class="label">Ignored Users List</td> 
+				<td class="data"><? 
+				foreach ($ignored_users as $user_id => $user_login) :
+				$ajax = "jQuery.post(IgnoreUser.ajaxurl,{'action':'bbp_5o1_unignore_user','data':'delete ".$user_id.";user_id ".$displayed_user_id."'},function(response){if (response){user=document.getElementById('ignored-user-".$user_id."');user.parentNode.removeChild(user);}});";
+				?>
+					<span id="ignored-user-<?php print $user_id; ?>" style="display: inline-block; line-height: 1.5em; margin: 3px; padding: 2px 5px; background: #eee;">
+						<span style="padding: 0 5px 0 0;"><?php print $user_login; ?></span>
+						<a style="cursor: pointer;" title="Remove" onclick="<?php print $ajax; ?>;">&#x2716;</a>
+					</span>
+				<?php endforeach; ?></td>
+			</tr>
+		</table><?php
+	}
+	
+	function show_ignore_user_button($author_link, $args) {
+		global $bbp;
+		$user_id = $bbp->current_user->ID;
+		$ignored_users = bbp_5o1_ignore_user::get_ignored_users($user_id);
+		$author_id = bbp_get_reply_author_id();
+		$reply_id = bbp_get_reply_id();
+		// So if it's not a reply we're adding it to,
+		// or if the reply author is the current user,
+		// or if the reply author can moderate the forum,
+		// then we don't show the link.
+		if ( !is_user_logged_in() || empty($reply_id) || $user_id == $author_id || user_can($author_id, 'moderate') )
+			return $author_link;
+		$elm_id = "ignore-user-".$author_id."-".$reply_id;
+		$ajax = "jQuery.post(IgnoreUser.ajaxurl,{'action':'bbp_5o1_ignore_user','data':'ignore ".$author_id.";user_id ".$user_id."'},function(response){if (response=='true'){elm=document.getElementById('".$elm_id."');elm.parentNode.removeChild(elm);}});";
+		$label = "Ignore User";
+		if ( !empty($ignored_users) && in_array($author_id, array_keys($ignored_users)) ) {
+			$ajax = "jQuery.post(IgnoreUser.ajaxurl,{'action':'bbp_5o1_unignore_user','data':'delete ".$author_id.";user_id ".$user_id."'},function(response){if (response=='true'){elm=document.getElementById('".$elm_id."');elm.parentNode.removeChild(elm);}});";
+			$label = "Unignore User";
+		}
+		$link .= '<a style="cursor: pointer;" id="'.$elm_id.'" onclick="'.$ajax.'">'.$label.'</a>';
+		return $author_link . '<br />' . $link;
+	}
+	
+	// The following two functions are probably a mess that should be cleaned up later.
+	function ajax_unignore_user() {
+		// data is a string like: 'delete 1;user_id 2'
+		// meaning delete the ignored user with id=1 from user=2's ignore list.
+		$data = $_POST['data'];
+		$parts = explode(";", $data);
+		foreach ($parts as $part) {
+			$piece = explode(" ", $part);
+			$command[$piece[0]] = $piece[1];
+		}
+		if ( !isset($command['user_id']) )
+			die('false');
+		// Here I should actually test to make sure that the two user ids are actually valid
+		// and that they exist in the database.  But I haven't done that yet.
+		$ignored_users = bbp_5o1_ignore_user::get_ignored_users($command['user_id']);
+		if ( isset($command['delete']) ) {
+			if ( empty($ignored_users) )
+				die('false');
+			$response = 'false';
+			if ( !in_array($command['delete'], array_keys($ignored_users) ) )
+				die('false');
+			if ( in_array($command['delete'], array_keys($ignored_users) ) )
+				$response = 'false';
+			$ignored_list = array();
+			foreach ($ignored_users as $user_id => $user_login) {
+				if ($command['delete'] == $user_id)
+					continue;
+				$ignored_list[$user_id] = $user_login;
+			}
+			$ignored_users = $ignored_list;
+			if ( empty($ignored_users) || !in_array($command['delete'], array_keys($ignored_users) ) ) {
+				$response = 'true';
+				bbp_5o1_ignore_user::set_ignored_users($command['user_id'], $ignored_users);
+			}
+		}
+		die($response);
+	}
+	
+	function ajax_ignore_user() {
+		// data is a string like: 'ignore 1;user_id 2'
+		// meaning add the user with id=1 to user=2's ignore list.
+		$data = $_POST['data'];
+		$parts = explode(";", $data);
+		foreach ($parts as $part) {
+			$piece = explode(" ", $part);
+			$command[$piece[0]] = $piece[1];
+		}
+		if ( !isset($command['user_id']) )
+			die('false');
+		if ( $command['user_id'] == $command['ignore'] )
+			die('false');
+		$ignored_users = bbp_5o1_ignore_user::get_ignored_users($command['user_id']);
+		if ( isset($command['ignore']) ) {
+			if ( user_can($command['ignore'], 'moderate') )
+				die('false');
+			$response = 'false';
+			if ( !empty($ignored_users) ) {
+				if (in_array($command['ignore'], array_keys($ignored_users) ) )
+					die('false');
+			}
+			if ( empty($ignored_users) || !in_array($command['ignore'], array_keys($ignored_users) ) ) {
+				$user = get_userdata($command['ignore']);
+				if ( !empty($user)) 
+					$ignored_users[$user->ID] = $user->user_login;
+			}
+			if ( in_array($command['ignore'], array_keys($ignored_users)) )  {
+				$response = 'true';
+				bbp_5o1_ignore_user::set_ignored_users($command['user_id'], $ignored_users);
+			}
+		}
+		die($response);
+	}
+	
+}
+ ?>
---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== bbPress Ignore User ===
+Contributors: master5o1
+Donate link: http://master5o1.com/donate/
+Tags: bbPress, bbPress 2.0, ignore user, block user, ignore, self-censorship, 5o1, master5o1
+Requires at least: 3.1 or higher
+Tested up to: 3.1.3
+Stable tag: 0.1
+
+Allow members of the forum to selectively have a user's posts and topics hidden from view.
+
+== Description ==
+
+Users can maintain their own personal ignore user list which is used to hide any posts or topics from that user.
+While viewing a topic, the current user can ignore any of the reply authors by clicking an 'Ignore User' link under/near the author name.  This adds the author to the user's ignore list.
+On the user's profile, the author can remove entries to their ignore list.
+
+The user can forcibly show all ignored posts or topics by viewing the topic or forum with `?show_ignored_users` applied to the url (a link to show all posts and topics is given when posts are hidden).
+
+Users who can moderate the forum cannot be ignored by any members and members cannot ignore themselves.
+
+== Installation ==
+
+1. Make sure you have bbPress 2.0 (or better) plugin activated.
+1. Upload `bbpress-ignore-users` folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Question =
+
+Answer
+
+== Screenshots ==
+
+1. Ignore/Unignore User link on the author side of a reply post.
+1. Topics hidden from listing.
+1. Ignore list can be maintained on the user's profile.
+
+== Changelog ==
+
+= 0.1 =
+
+* First release.
+
+== Upgrade Notice ==
+
+= 0.1 =
+
+* None
